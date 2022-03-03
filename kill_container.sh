bd10c3fd4f0d64fbcda7688d291abb9d79594d5e
harshneosoft <harshneosoft@gmail.com>
2022-03-03 16:15:19 +0530

    some changes done

--- kill_container.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo su
-
-docker rm -f staticsite || true
--- start_container.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo su
-
-docker run -d -P --name staticsite staticsite:latest
-
-docker port staticsite
